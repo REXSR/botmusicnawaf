51c2cd39644d604c3b4d6fc98d996693e5136985
REXSR <41483292+REXSR@users.noreply.github.com>
2018-11-18 04:03:02 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NTEyNTk3ODM4MjgzMTQ1MjI3.DtJLng.IWNN3f5jKMp95Mb9Gzk7H3BI6YA'; // ya wr3 la tns5
+
+exports.PREFIX = 'Na';
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
